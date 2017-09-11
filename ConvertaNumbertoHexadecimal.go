e82c1fad07d01fcc08e57667ae8a5339cf1f7335
luo jiyin <luojiyin@hotmail.com>
2017-09-11 21:26:37 +0800

    Update ConvertaNumbertoHexadecimal.go

--- ConvertaNumbertoHexadecimal.go
+++ ConvertaNumbertoHexadecimal.go
@@ -8,7 +8,17 @@ func toHex(num int) string { //what about int ? is int32  int64 ???
     
     var result string
     
-    Hexnum  := [16]string {"0", "1","2","3","4","5","6","7","8","9","a", "b","c","d","e","f"}
+    
+    
+//func getHexDigit(a int) (hex string){
+//	if a < 10 {
+//		return strconv.Itoa(a)
+//	}
+//
+//	return string([]byte{'a' + byte(a - 10)})
+//}
+    Hexnum  := [16]string {"0", "1","2","3","4","5","6","7","8","9","a", "b","c","d","e","f"} //which  better
+    
     if num < 0 {
         num = 16 * 16 * 16 * 16 * 16 * 16 * 16 * 16  + num 
      }
