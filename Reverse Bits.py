4a63cf0f7ea71336512097a7d6212072be645af8
luo jiyin <luojiyin@hotmail.com>
2017-09-28 09:31:52 -0500

    Create Reverse Bits.py

---
+++ Reverse Bits.py
@@ -0,0 +1,11 @@
+class Solution:
+    # @param n, an integer
+    # @return an integer
+    def reverseBits(self, n):
+        ans = 0 
+        for i in xrange(32):
+            ans<<=1
+            ans |= n & 1
+            n >>= 1
+        return ans
+       //神奇的位运算
