a83f595168b1d0f7c0f6e33f35ade1ec3d4077fd
luo jiyin <luojiyin@hotmail.com>
2017-09-29 08:27:00 -0500

    Update K-diff Pairs in an Array.go

--- K-diff Pairs in an Array.go
+++ K-diff Pairs in an Array.go
@@ -52,3 +52,14 @@ func findPairs(nums []int, k int) int {
     
     return c
 }
+//---------------------------Two-pointer Approach
+public int findPairs(int[] nums, int k) {
+    int ans = 0;
+    Arrays.sort(nums);
+    for (int i = 0, j = 0; i < nums.length; i++) {
+        for (j = Math.max(j, i + 1); j < nums.length && (long) nums[j] - nums[i] < k; j++) ;
+        if (j < nums.length && (long) nums[j] - nums[i] == k) ans++;
+        while (i + 1 < nums.length && nums[i] == nums[i + 1]) i++;
+    }
+    return ans;
+}
