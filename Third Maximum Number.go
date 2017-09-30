a9b364c4376e4ff511306f6c09051d1db82ffa85
luo jiyin <luojiyin@hotmail.com>
2017-09-30 10:47:42 +0800

    Update Third Maximum Number.go

--- Third Maximum Number.go
+++ Third Maximum Number.go
@@ -27,4 +27,22 @@ func thirdMax(nums []int) int {
 }
 //不能像Pytohon 那样那样把值初始化为  None，不断比较，更新 first second   third 的值， 最后，如果third 为None，
 没有第三大值，返回first，third不为 None， 则返回third
-所以要把值初始化为-2147483648  ， int32中 最小值，然后 
+所以要把值初始化为-2147483648  ， int32中 最小值， 
+
+///-----------------------  代码思路很清晰
+func thirdMax(nums []int) int {
+    sort.Ints(nums)
+    count := 1
+    max := nums[len(nums) - 1]
+    max_num := nums[len(nums) - 1]
+    for i := len(nums) -1 ; i >= 0 ; i--{
+        if max > nums[i]{
+            max = nums[i]
+            count = count + 1
+        }
+        if count == 3{
+            return nums[i]
+        }
+    }
+    return max_num
+}
